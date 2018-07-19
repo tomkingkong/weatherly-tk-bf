d2fc80a8de97c90fd744cf16eb691ca4934981c7
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-07-18 20:50:40 -0600

    Import api key to app and fetch call to denver object need to make it dynamic based on user input

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header.js'
 import Search from './Search.js'
-import CurrentWeather from './CurrentWeather.js'
+import CurrentWeather from './CurrentWeather.js';
+import Key from './Key.js'
 
 
 
 class App extends Component {
+
+  componentDidMount() {
+    fetch(`http://api.wunderground.com/api/${Key}/geolookup/conditions/hourly/forecast10day/q/denver.json`) //add user input in place of Denver.
+    .then(response => response.json()).then(data => console.log(data));
+  }
+
   render() {
     return (
       <div className="App">
