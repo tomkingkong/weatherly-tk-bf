0e1e2814f1fc0c794ac2fc0f8ead914dd6b52f5f
Tom King <design.tomking@gmail.com>
2018-07-19 13:47:26 -0600

    Merge master

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import Header from './Header.js'
 import Search from './Search.js'
-<<<<<<< HEAD
 import CurrentWeather from './CurrentWeather.js'
 import Key from './Key.js'
-
-=======
-import CurrentWeather from './CurrentWeather.js';
-import Key from './Key.js'
->>>>>>> b317531c1fe2a820267d7aeff89e9f66365a2b8e
 
 
 
 class App extends Component {
-<<<<<<< HEAD
   constructor() {
     super();
 
@@ -30,15 +23,6 @@ class App extends Component {
     .then(response => response.json()).then(data => console.log(data));
   }
 
-=======
-
-  componentDidMount() {
-    fetch(`http://api.wunderground.com/api/${Key}/conditions/q/co/Denver.json`) //add user input in place of Denver.
-    .then(response => response.json()).then(data => console.log(data));
-  }
-
-
->>>>>>> b317531c1fe2a820267d7aeff89e9f66365a2b8e
   render() {
     return (
       <div className="App">
