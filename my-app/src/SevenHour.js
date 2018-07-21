dc6a74e5f82fb8cca7b14e183655d429c5f6e206
Tom King <design.tomking@gmail.com>
2018-07-21 09:46:16 -0600

    Sevenhour component maps out seven cards

--- my-app/src/SevenHour.js
+++ my-app/src/SevenHour.js
@@ -4,15 +4,18 @@ import Card from './Card.js';
 import './SevenHour.css';
 
 const SevenHour = (props) => {
+  let hourlyArray = props.hourlyArray.slice(0, -29);
+
   return (
     <div className="SevenHour">
       {
-        props.hourlyArray.map(hour => {
-          return ( <Card 
-            hour={hour.FCTTIME.civil}
-            icon={hour.icon_url}
-            temp={hour.temp.english}
-          />
+        hourlyArray.map(hour => {
+          return ( 
+            <Card 
+              hour={hour.FCTTIME.civil}
+              icon={hour.icon_url}
+              temp={hour.temp.english}
+            />
           )
         })
       }
