fd543a5483b5875941b535017c6c10e626de84e6
Tom King <design.tomking@gmail.com>
2018-07-25 11:07:43 -0600

    Current weather component loads only if data exists
    Eliminating lonely characters without their variable partners

--- my-app/src/CurrentWeather.js
+++ my-app/src/CurrentWeather.js
@@ -17,23 +17,25 @@ const CurrentWeather = (props) => {
 
   return (
     <React.Fragment>
-    <section className="CurrentWeather">
-    <h3>{currentCity}, {currentState}</h3>
-      <div className="currentData">
-      <div className="left-section">
-        <p>{currentCondition}</p>
-        <h4>{currentDay}</h4>
-      </div>
-      <div className="mid-section">
-        <img alt={currentCondition} src={currentIcon} />
-      </div>
-      <div className="right-section">
-        <p className="currentTemp">{currentTemp}°</p>
-        <p>{currentHigh}° / {currentLow}°</p>
-      </div>
-      </div>
-    <p className="summary">{summary}</p>
-    </section>
+      { currentIcon &&
+      <section className="CurrentWeather">
+      <h3>{currentCity}, {currentState}</h3>
+        <div className="currentData">
+        <div className="left-section">
+          <p>{currentCondition}</p>
+          <h4>{currentDay}</h4>
+        </div>
+        <div className="mid-section">
+          <img alt={currentCondition} src={currentIcon} />
+        </div>
+        <div className="right-section">
+          <p className="currentTemp">{currentTemp}°</p>
+          <p>{currentHigh}° / {currentLow}°</p>
+        </div>
+        </div>
+      <p className="summary">{summary}</p>
+      </section>
+      }
     </React.Fragment>
   )
 }
