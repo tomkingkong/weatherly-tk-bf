c7a904700d0938dfc1f893c97dfa91b566ac5c03
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-07-26 08:44:32 -0600

    Add tests for rendering tenday and sevenhour data

--- my-app/src/Card.test.js
+++ my-app/src/Card.test.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import { shallow, mount } from 'enzyme';
 import Card from './Card';
+// import mockHourly from './mockHourly.js';
+// import mockDayArray from './mockDayArray.js';
+
 
 
 describe('Card', () => {
@@ -23,5 +26,34 @@ describe('Card', () => {
     expect(wrapper.find('article').length).toEqual(1);
     expect(wrapper.find('img').length).toEqual(1);
   })
+
+  it('should render sevenHour data', () => {
+    let hourObject = { 
+      hour: '12PM',
+      icon: 'partlycloudy.svg',
+      temp: 47,
+      condition: 'Partly Cloudy',
+    }
+    wrapper = shallow(<Card 
+                      hour={hourObject.hour} 
+                      icon={hourObject.icon} 
+                      temp={hourObject.temp} 
+                      condition={hourObject.condition} 
+                      />)
+    expect(wrapper.find('p').first().text()).toEqual('12PM');
+  })
+
+  // it('should call toggleHours when card is clicked', () => {
+  //   wrapper = shallow(<Card />);
+
+  //   wrapper.instance().toggleHours = jest.fn();
+    
+  //   let card = wrapper.find('article');
+
+  //   card.simulate('click');
+
+  //   expect(wrapper.instance().toggleHours).toHaveBeenCalled();
+  //   expect(wrapper.instance().toggleHours).toHaveBeenCalledTimes(1);
+  // })
 
 })
